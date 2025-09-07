3a2badf1bff4dea85738bc90dc3db9c0c10f9702
qwa406495-sketch <qwa406495@gmail.com>
2025-09-07 13:48:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import steamlit as st
+
+st.title("stress detector app")
+
+#taking users input
+name=st.text_input("enter your name")
+
+#display a message when a button is clicked 
+if st.button("submit")
+  st.write(f"hlo, {name}! welcome to stress detector app.")
+  
