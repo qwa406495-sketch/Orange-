50fe2d81a800682daf64ad0f9eb5cfda239c72e5
qwa406495-sketch <qwa406495@gmail.com>
2025-09-07 15:21:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,7 @@ name=st.text_input("enter your name")
 if st.button("submit"):
   st.write(f"hlo, {name}! welcome to stress detector app.")
   st.write("please choose the correct options")
-  
+  sleep = st.number_input("enter your sleeping hours", min_value=0.0, step=0.1)
+  if st.button("percentage of stress"):
+    predicted_stress = model.predict([hours])[0]
+    st.success(f"predicted stress: {predicted_stress:.2f}")
