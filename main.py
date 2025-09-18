0184d3cdd4be4fbb63b956d032dcb8b7f0e217e2
qwa406495-sketch <qwa406495@gmail.com>
2025-09-18 13:51:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ name=st.text_input("enter your name")
 #display a message when a button is clicked 
 if st.button("submit"):
   st.write(f"hlo, {name}! welcome to stress detector app.")
+  st.write(f"please choose the correct option")
+  if st.button("let's start!!")
+  
